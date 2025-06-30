55d826edabf3428120d5daddcca08994626ebe8a
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-06-30 14:54:41 -0300

    feat: create camp owner and generate data random  in django

--- project/settings.py
+++ project/settings.py
@@ -110,7 +110,7 @@ LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'America/Sao_Paulo'
 
-USE_I18N = True
+USE_I18N = False
 
 USE_TZ = True
 
@@ -122,6 +122,10 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = (
     BASE_DIR / 'base_static',
 )
+STATIC_ROOT = BASE_DIR / 'static'  # collectstatic
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
